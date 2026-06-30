0cdddda4bfc333b9c4a611952fcf9befcef02f5f
Dev.Kasra <ksrybharwnd@gmail.com>
2026-06-30 16:33:40 +0330

    wf

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
-import Login from "./pages/Login/loginpage/Login"
-import Signup from "./pages/Signup/Signup"
+
 import "../src/App.css"
 
 function App() {
 
   return (
     <>
-      <Login/>
-      {/* <Signup/> */}
+
     </>
   )
 }
